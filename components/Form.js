db86b3d85669846064f8a816d5279b83021b5303
Erick Camacho <ercamdev@gmail.com>
2021-02-12 21:52:11 -0600

    Add Emoji-Mart & create functions to handle emojis

--- components/Form.js
+++ components/Form.js
@@ -1,7 +1,9 @@
 import { useEffect, useState, useRef } from 'react'
 import firebase from 'firebase/app'
 import 'firebase/firestore'
-import { Send } from 'react-feather'
+import { Send, Smile } from 'react-feather'
+import 'emoji-mart/css/emoji-mart.css'
+import { Picker } from 'emoji-mart'
 
 const db = firebase.firestore()
 const messagesRef = db.collection("messages")
@@ -22,14 +24,25 @@ const addMessage = (message, user) => {
 
 const Form = ({user}) => {
   const [message, setMessage] = useState("")
+  const [open, setOpen] = useState(false)
+  
   const inputRef = useRef(null)
 
   useEffect(() => {
     inputRef.current.focus()
-  }, [])
+  }, [open])
 
   const handleChange = (e) => {
     setMessage(e.target.value)
+  }
+
+  const handleClick = () => {
+    setOpen(!open)
+  }
+
+  const addEmoji = (emoji) => {
+    setMessage(message + emoji.native)
+    inputRef.current.focus()
   }
   
   const handleSubmit = (e) => {
@@ -38,28 +51,48 @@ const Form = ({user}) => {
     if (trimmedMessage){
       addMessage(trimmedMessage, user)
     }
+    setOpen(false)
     setMessage("")
+  }
+
+  const handleKeyUp = (e) => {
+    if (e.key === "Escape") {
+      setOpen(false)
+    }
   }
   
   return (
     <footer className="py-2 lg:py-3 px-4 shadow">
-      <form onSubmit={handleSubmit} className="flex max-w-3xl mx-auto">
-        <input 
-          ref={inputRef}
-          type="text" 
-          placeholder="Type a message"
-          value={message}
-          onChange={handleChange}
-          className="bg-gray-200 placeholder-gray-500 rounded-3xl py-2 pl-4 flex-1 focus:outline-none"
-        />
-        <button 
-          type="submit"
-          disabled={!message}
-          className="self-center text-gray-500 disabled:opacity-50 ml-2 focus:outline-none"
+      <div className="flex max-w-3xl mx-auto md:relative">
+        <div className={`absolute bottom-14 md:bottom-12 left-0 ${open || 'hidden'}`}>
+          <Picker native={true} emoji="" color="#3B82F6" enableFrequentEmojiSort={true} showPreview={false} showSkinTones={false} perLine={9} onClick={addEmoji}/>
+        </div>
+        <button
+          type="button"
+          onClick={handleClick}
+          className={`self-center text-gray-500 focus:outline-none ${open && 'text-blue-500'}`}
         >
-          <Send size={24}/>
+          <Smile size={24}/>
         </button>
-      </form>
+        <form onSubmit={handleSubmit} className="flex flex-1">
+          <input 
+            ref={inputRef}
+            type="text" 
+            placeholder="Type a message"
+            value={message}
+            onChange={handleChange}
+            onKeyUp={handleKeyUp}
+            className="bg-gray-200 placeholder-gray-500 rounded-3xl py-2 pl-4 flex-1 focus:outline-none mx-2"
+          />
+          <button 
+            type="submit"
+            disabled={!message}
+            className="self-center text-gray-500 disabled:opacity-50 focus:outline-none"
+          >
+            <Send size={24}/>
+          </button>
+        </form>
+      </div>
     </footer>
   );
 }
--- components/Message.js
+++ components/Message.js
@@ -18,7 +18,7 @@ const Message = ({ text, createdAt, displayName, photoURL }) => {
       </div>
       <div className="flex flex-col">
         <div className="col-span-2 flex flex-wrap">
-          {displayName ? <p className="font-bold mr-2">{displayName}</p> : null}
+          {displayName ? <p className="font-bold mr-2 truncate">{displayName}</p> : null}
           {createdAt?.seconds ? (
             <span className="text-gray-500 text-xs self-center">{formatDate(new Date(createdAt.seconds * 1000))}</span>
           ) : null}
